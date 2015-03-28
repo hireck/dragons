266347d918d52048059c6114aa3f0a3c1beb1ab1
hireck <reckmanhgb@gmail.com>
2015-03-28 19:30:45 +0100

    Update dragon_farming.py

--- dragon_farming.py
+++ dragon_farming.py
@@ -1,12 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
+from operator import itemgetter
 
 
-################ THE MODEL ################
+#**************** TOOLS ***********************
+
+#giving structure to an input parameter:
+class InputParam(object):
+  def __init__(self, cat, shortname, longname, unit, minim, maxim):
+        self.cat = cat
+        self.shortname = shortname
+        self.longname = longname
+        self.unit = unit
+        self.min = float(minim)
+        self.max = float(maxim)
+        #self.mean = float(ave)
+  def generate_values(self, n):
+        self.values = np.random.uniform(self.min, self.max, n)
+        
+#creating structured input parameters (from a list that was read from a file) and putting them into a dictionary:
+def prepare_input(input_data, input_cats):
+        for l in input_data:
+                infos = l.strip().split('\t')
+                ip = InputParam(*infos)
+                ip.generate_values(n)
+                input_cats[ip.cat][ip.shortname] = ip 
+                
+#giving structure to an output parameter:
+class OutputParam(object):
+  def __init__(self, shortname, plot_title, y_axis_name, unit):
+        self.shortname = shortname
+        self.plot_title = plot_title
+        self.y_label = y_axis_name
+        self.unit = unit
+        #self.fuction = function
+    #def calculate_impact(self):
+        #self.values = self.function(*args)
+                
+                
+#*************** THE MODEL *********************
 
 def cal_num_knights(dragons,knight_population,food_factor):
 
-  knights = knight_population - dragons[number].values * food_factor * dragons[weigth].values + knight_population*(1.-1./np.log10(dragons[age].values))
+  knights = knight_population - dragons['number'].values * food_factor * dragons['weigth'].values + knight_population*(1.-1./np.log10(dragons['age'].values))
   
   return knights
 
@@ -14,124 +54,69 @@ def cal_num_knights(dragons,knight_population,food_factor):
 
 def cal_dragon_output(dragons,knight_population,knights_left):
 
-  digestion_efficiency = dragons[age].values * dragons[weigth].values
+  digestion_efficiency = dragons['age'].values * dragons['weigth'].values
 
   digested_knights_products = (knight_population - knights_left)/digestion_efficiency
   
   return digested_knights_products
 
 
-############## TOOLS TO FETCH AND PREPARE INPUT ###########
+        
 
 
-class InputParam(object):
-  def __init__(self, cat, shortname, longname, unit, minim, maxim):
-  	self.cat = cat
-  	self.shortname = shortname
-  	self.longname = longname
-  	self.unit = unit
-  	self.min = float(minim)
-  	self.max = float(maxim)
-  	#self.mean = float(ave)
-  def generate_values(self, n):
-  	self.values = np.random.uniform(self.min, self.max, n)
+#***************** LOAD INPUT ***********************
 
-def prepare_input(input_data, input_cats):
-	for l in input_data:
-		infos = l.strip().split('\t')
-		ip = InputParam(*infos)
-		ip.generate_values(n)
-		input_cats[ip.cat][ip.shortname] = ip 
+#set the number of repetitions: 
+n = 1000       
 
-########### LOAD INPUT ###################
- 
-n = 1000 	
+#set any fixed values:
 initial_knight_population = 15430
 food_factor = 10.
+
+#initialize a dictionary to hold all your input data, organized by category:
 input_cats = defaultdict(dict)
 
-  	
+# read in the data from a file:       
 input_data = open('dragon_farm_input.txt', 'r').readlines()[1:]
-prepare_input(input_data)
+#structure it and put it into the input_cats dictionary:
+prepare_input(input_data, input_cats) 
 
 
-########### RUN THE MODEL ###########
+#***************** RUN THE MODEL *********************
 
 dragons = input_cats['dragons']
 #knights = input_cats['knights'] #not used right now, as we have only 'initial knight population' with a fixed value
 #but you could add this one and other knight parameters to the input file, just like the dragons, 
 #e.g. knight running speed, knight armor budget, ....
 
+#initialize a dictionary to hold the output of the calculations:
+impact_factors = {}
 
+#create the output parameter 'knights_left':
+impact_factors['knights_left'] = OutputParam('knights_left', 'Knight population', 'Knights left', '')
+#calculate the values for this output parameter:
+impact_factors['knights_left'].values = cal_num_knights(dragons,initial_knight_population,food_factor) 
 
-knights_left = cal_num_knights(input_params,initial_knight_population,food_factor) 
+knights_left = impact_factors['knights_left'].values
 
-dragon_leftovers = cal_dragon_output(input_params,initial_knight_population,knights_left)
+impact_factors['dragon_output'] = OutputParam('dragon_output', 'Dragon waste products', 'Dragon output', 'kg')
+impact_factors['dragon_output'].values = cal_dragon_output(dragons,initial_knight_population, knights_left)
 
+#*********** PLOT *************
 
-plt.subplot(2, 2, 1)
-plt.title('Knight population')
-plt.plot(dragons.number[4],knights_left,'ro')
-plt.xlabel(dragons.number[0])
-plt.ylabel('Kinghts left')
-
-plt.subplot(2, 2, 2)
-plt.plot(dragons.length[4],knights_left,'ro')
-plt.xlabel(dragons.length[0])
-plt.ylabel('Kinghts left')
-
-plt.subplot(2, 2, 3)
-plt.plot(dragons.weigth[4],knights_left,'ro')
-plt.xlabel(dragons.weigth[0])
-plt.ylabel('Kinghts left')
-
-plt.subplot(2, 2, 4)
-plt.plot(dragons.age[4],knights_left,'ro')
-plt.xlabel(dragons.age[0])
-plt.ylabel('Kinghts left')
-
-plt.show()
-
-
-
-
-plt.subplot(2, 2, 1)
-ylabel1 = 'Dragon output [kg]'
-plt.title('Dragon leftovers')
-plt.plot(dragons.number[4],dragon_leftovers,'ro')
-plt.xlabel(dragons.number[0])
-plt.ylabel(ylabel1)
-
-plt.subplot(2, 2, 2)
-plt.plot(dragons.length[4],dragon_leftovers,'ro')
-plt.xlabel(dragons.length[0])
-plt.ylabel(ylabel1)
-
-
-plt.subplot(2, 2, 3)
-plt.plot(dragons.weigth[4],dragon_leftovers,'ro')
-plt.xlabel(dragons.weigth[0])
-plt.ylabel(ylabel1)
-
-plt.subplot(2, 2, 4)
-plt.plot(dragons.age[4],dragon_leftovers,'ro')
-plt.xlabel(dragons.age[0])
-plt.ylabel(ylabel1)
-
-plt.show()
-
-#print knights_left
-
-#print dragon_leftovers
-
-#print dragons.number[4],knights_left
-
-
-
-#for attr, value in dragons.__dict__.iteritems():
-#       print attr
-
-#a = dir(dragons) if not dragons.startswith('__')
-
-#[i for i in dir(dragons) if not dragons.startswith('__')]
+for ic in input_cats:
+    incat = input_cats[ic]
+    for factor in impact_factors:
+        outpar = impact_factors[factor]
+        sorted_inparams = sorted(incat.keys())
+        for i, key in enumerate(sorted_inparams):
+            inpar = incat[key]
+            rows = len(sorted_inparams) / 2
+            plt.subplot(2, 2, i) #which of the 2's indicates the number of rows?
+            #if i == 0:
+                #plt.title(outpar.plot_title)
+            plt.plot(inpar.values, outpar.values, 'ro')
+            plt.xlabel(inpar.longname)
+            plt.ylabel(outpar.y_label)
+        plt.show()
 
